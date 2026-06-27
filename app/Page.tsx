11e55c03156d74f64d018691daace38cf85c07c6
pathakvicky-1006 <pathakvicky.098@gmail.com>
2026-06-27 12:52:26 +0530

    Update Page.tsx

--- app/Page.tsx
+++ app/Page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div style={{background:'#0D1117', minHeight:'100vh', color:'white', padding:'20px', textAlign:'center'}}>
       <h1 style={{color:'#FF6B00', fontSize:'32px', marginTop:'100px'}}>Elite FF Arena </h1>
       <p style={{marginTop:'20px', fontSize:'18px'}}>UPI: tera@paytm</p>
-      <p style={{marginTop:'40px'}}>Tournament Live Ho Gaya ✅</p>
+      <p style={{marginTop:'40px'}}>Tournament Live Ho Gaya </p>
     </div>
   )
 }
